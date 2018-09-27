9d9eb96b70da77b58b65f0d9f2e3cb5d0ca25270
nazarozz <yulianazarova@inbox.ru>
2018-09-27 17:26:16 +0300

    Написали асинхронный Ajax запрос на сервер

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+var HttpClient = function HttpClient() {
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function () {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
+    };
+    anHttpRequest.open("GET", aUrt, true);
+    anHttpRequest.send(null);
+  };
+};
+
+var client = new HttpClient();
+
+var showStat = function showStat() {
+  client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyCUrZ8fPXGj3hwPtuZdrrElaUY6g62UhFs', function (Response) {
+    console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+  });
+};
+
+showStat();
---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+var HttpClient = function HttpClient(){
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function () {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
+    };
+    anHttpRequest.open("GET", aUrt, true);
+    anHttpRequest.send(null);
+  };
+};
+
+let client = new HttpClient();
+
+let showStat = () => {
+  client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyCUrZ8fPXGj3hwPtuZdrrElaUY6g62UhFs', (Response)=>{
+    console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+  })
+}
+
+showStat();
+
