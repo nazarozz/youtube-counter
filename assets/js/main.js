f29345c3df684ff87bb66455f7d8e17705b2cdc3
nazarozz <yulianazarova@inbox.ru>
2018-09-28 11:56:04 +0300

    Оформлена карточка с каналом

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var HttpClient = function HttpClient() {
   this.get = function (aUrl, aCallback) {
@@ -6,16 +6,27 @@ var HttpClient = function HttpClient() {
     anHttpRequest.onreadystatechange = function () {
       if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
     };
-    anHttpRequest.open("GET", aUrt, true);
+    anHttpRequest.open("GET", aUrl, true);
     anHttpRequest.send(null);
   };
 };
 
 var client = new HttpClient();
+var idChannel = 'UCVswRUcKC-M35RzgPRv8qUg';
+var part = 'statistics,brandingSettings';
+var nameChannel = document.querySelector('#name-channel');
+var descChannel = document.querySelector('#description-channel');
+var imgChannel = document.querySelector('#img-channel');
+var countChannel = document.querySelector('#count');
 
 var showStat = function showStat() {
-  client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyCUrZ8fPXGj3hwPtuZdrrElaUY6g62UhFs', function (Response) {
-    console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+  client.get('https://www.googleapis.com/youtube/v3/channels?part=' + part + '&id=' + idChannel + '&key=AIzaSyCUrZ8fPXGj3hwPtuZdrrElaUY6g62UhFs', function (response) {
+    var info = JSON.parse(response).items[0];
+    console.log(info);
+    nameChannel.innerText = info.brandingSettings.channel.title;
+    descChannel.innerText = info.brandingSettings.channel.description;
+    countChannel.innerText = info.statistics.subscriberCount;
+    imgChannel.src = info.brandingSettings.image.bannerImageUrl;
   });
 };
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,30 @@
-var HttpClient = function HttpClient(){
+var HttpClient = function HttpClient() {
   this.get = function (aUrl, aCallback) {
     var anHttpRequest = new XMLHttpRequest();
     anHttpRequest.onreadystatechange = function () {
       if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200) aCallback(anHttpRequest.responseText);
     };
-    anHttpRequest.open("GET", aUrt, true);
+    anHttpRequest.open("GET", aUrl, true);
     anHttpRequest.send(null);
   };
 };
 
 let client = new HttpClient();
+let idChannel = 'UCVswRUcKC-M35RzgPRv8qUg';
+let part = 'statistics,brandingSettings';
+let nameChannel = document.querySelector('#name-channel');
+let descChannel = document.querySelector('#description-channel');
+let imgChannel = document.querySelector('#img-channel');
+let countChannel = document.querySelector('#count');
 
 let showStat = () => {
-  client.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=AIzaSyCUrZ8fPXGj3hwPtuZdrrElaUY6g62UhFs', (Response)=>{
-    console.log(JSON.parse(response).items[0].statistics.subscriberCount);
+  client.get(`https://www.googleapis.com/youtube/v3/channels?part=${part}&id=${idChannel}&key=AIzaSyCUrZ8fPXGj3hwPtuZdrrElaUY6g62UhFs`, (response) => {
+    let info = JSON.parse(response).items[0];
+    console.log(info);
+    nameChannel.innerText = info.brandingSettings.channel.title;
+    descChannel.innerText = info.brandingSettings.channel.description;
+    countChannel.innerText = info.statistics.subscriberCount;
+    imgChannel.src = info.brandingSettings.image.bannerImageUrl;
   })
 }
 
